abc374e4ebfa20cca1d4ffdcb5bf559ee31a723a
HYERINI <rinlee2001@naver.com>
2024-07-04 23:42:23 +0900

    7/4 실습 내용

--- 바닐라js로 크롬앱 만들기/js/todo.js
+++ 바닐라js로 크롬앱 만들기/js/todo.js
@@ -2,11 +2,30 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+// 입력한 투두리스트를 저장할 배열
+const TODOS_KEY = "todos";
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+const toDos = JSON.parse(savedToDos);
+
+// 투두리스트를 저장할 함수
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+function deleteToDo(){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
+    const button = document.createElement("button");
     span.innerText = newTodo;
+    button.innerText = '❎';
+    button.addEventListener("click", deleteToDo);
     li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
@@ -16,6 +35,16 @@ function handleToDoSubmit(event){
     toDoInput.value = "";
 
     paintToDo(newToDo);
+    toDos.push(newToDo);
+    saveToDos();
 }
 
-toDoForm.addEventListener("submit", handleToDoSubmit)
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+
+if(savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach(todo => {
+        paintToDo(todo)
+    })
+}
