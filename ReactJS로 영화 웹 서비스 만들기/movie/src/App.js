a4bd80207954c8f37b3e392699c86ecbf00e8fb4
HYERINI <rinlee2001@naver.com>
2024-08-07 21:51:54 +0900

    8/7 공부내용

--- ReactJS로 영화 웹 서비스 만들기/movie/src/App.js
+++ ReactJS로 영화 웹 서비스 만들기/movie/src/App.js
@@ -1,13 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function App() {
-    const [counter, setValue] = useState();
+function App(){
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year")
+            .then(response => response.json())
+            .then((json) => {
+                setLoading(false);
+                setMovies(json.data.movies);
+            })
+    }, [])
 
-    return (
-    <div>
-        <h1>Welcome back</h1>
-    </div>
-    );
+    return(
+        <div>
+            <h1>Movies 보여줄랭</h1>
+            <ul>
+            {
+                movies.map((movie, index) =>
+                    (
+                        <div>
+                            <li key={movies.index}>
+                                {movie.title}
+                            </li>
+                            <img src={movie.medium_cover_image} />
+                        </div>
+                    )
+                )
+            }
+            </ul>
+        </div>
+    )
 }
 
 export default App;
